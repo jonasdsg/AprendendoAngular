1f99e47498b2506f4cb47968e2c97c1212ae25b0
Jonas Góes <jonasdossantos1991@gmail.com>
2020-10-23 21:22:15 -0300

    Escutando eventos

---
+++ src/app/aulas/loiane/listening-events/listening-events.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'loiane-listening-events',
+    templateUrl: './listening-events.component.html'
+})
+export class ListeningEventsComponent{
+
+}
--- src/app/aulas/loiane/loiane.module.ts
+++ src/app/aulas/loiane/loiane.module.ts
@@ -3,11 +3,22 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { StyleBindingComponent } from './style-binding/style-binding.component';
+import { ListeningEventsComponent } from './listening-events/listening-events.component';
 
 @NgModule({
-    declarations:[StyleBindingComponent],
-    exports:[StyleBindingComponent],
-    imports:[FormsModule,CommonModule]
+    declarations:[
+        StyleBindingComponent,
+        ListeningEventsComponent,
+    ],
+    exports:[
+        StyleBindingComponent,
+        ListeningEventsComponent
+
+    ],
+    imports:[
+        FormsModule,
+        CommonModule
+    ]
 
 })
 export class LoianeModule{}
