175f6b844a9269b730461e9720c50cc48c8b8b3c
Jonas Góes <jonasdossantos1991@gmail.com>
2020-10-25 19:53:07 -0300

    Two way data binding com ngModel

--- src/app/aulas/loiane/loiane.module.ts
+++ src/app/aulas/loiane/loiane.module.ts
@@ -4,15 +4,18 @@ import { CommonModule } from '@angular/common';
 
 import { StyleBindingComponent } from './style-binding/style-binding.component';
 import { ListeningEventsComponent } from './listening-events/listening-events.component';
+import { TwoWayDataBindingComponent } from './too-way-data-binding/two-way-data-binding.component';
 
 @NgModule({
     declarations:[
         StyleBindingComponent,
         ListeningEventsComponent,
+        TwoWayDataBindingComponent,
     ],
     exports:[
         StyleBindingComponent,
-        ListeningEventsComponent
+        ListeningEventsComponent,
+        TwoWayDataBindingComponent,
 
     ],
     imports:[
---
+++ src/app/aulas/loiane/too-way-data-binding/two-way-data-binding.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'loiane-two-way',
+    templateUrl: './two-way-data-binding.component.html'
+})
+export class TwoWayDataBindingComponent{
+    public variavel:string = '';
+}
